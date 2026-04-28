6e8182f5d67d7f2e8b9b67ae65d66eec38724a27
MagnusJhang <htshake@gmail.com>
2026-04-28 12:36:36 +0800

    fix: Precision align MS OS 2.0 descriptors to 162 bytes and use standard MS UUID

--- src/usb_descriptors.c
+++ src/usb_descriptors.c
@@ -12,7 +12,7 @@ tusb_desc_device_t const desc_device = {
     .bDeviceProtocol    = 0x00,
     .bMaxPacketSize0    = CFG_TUD_ENDPOINT0_SIZE,
     .idVendor           = 0xCafe,
-    .idProduct          = 0x4910, // Brand new PID
+    .idProduct          = 0x4912, // New PID
     .bcdDevice          = 0x0100,
     .iManufacturer      = 0x01,
     .iProduct           = 0x02,
@@ -25,7 +25,7 @@ uint8_t const * tud_descriptor_device_cb(void) {
 }
 
 //--------------------------------------------------------------------+
-// Configuration Descriptor (No IAD, back to pure Vendor)
+// Configuration Descriptor
 //--------------------------------------------------------------------+
 uint8_t const desc_configuration[] = {
     TUD_CONFIG_DESCRIPTOR(1, 1, 0, TUD_CONFIG_DESC_LEN + TUD_VENDOR_DESC_LEN, 0, 100),
@@ -38,14 +38,14 @@ uint8_t const * tud_descriptor_configuration_cb(uint8_t index) {
 }
 
 //--------------------------------------------------------------------+
-// String Descriptors (MS OS 1.0 Support)
+// String Descriptors (MS OS 1.0 Support for backward compatibility)
 //--------------------------------------------------------------------+
 char const* string_desc_arr [] = {
     (const char[]) { 0x09, 0x04 }, 
     "TinyUSB",                     
     "Pico Bulk Echo",       
     "123456",                      
-    "MSFT100\x01\x01", // MS OS 1.0 Index 0xEE (WinUSB Trigger)
+    "MSFT100\x01\x01", 
 };
 
 static uint16_t _desc_str[32];
@@ -55,13 +55,11 @@ uint16_t const* tud_descriptor_string_cb(uint8_t index, uint16_t langid) {
     if (index == 0) {
         memcpy(&_desc_str[1], string_desc_arr[0], 2);
         chr_count = 1;
-    } 
-    else if (index == 0xEE) {
+    } else if (index == 0xEE) {
         const char* str = string_desc_arr[4];
         chr_count = strlen(str);
         for (uint8_t i=0; i<chr_count; i++) _desc_str[1+i] = str[i];
-    }
-    else {
+    } else {
         if (!(index < sizeof(string_desc_arr)/sizeof(string_desc_arr[0]))) return NULL;
         const char* str = string_desc_arr[index];
         chr_count = strlen(str);
@@ -73,20 +71,20 @@ uint16_t const* tud_descriptor_string_cb(uint8_t index, uint16_t langid) {
 }
 
 //--------------------------------------------------------------------+
-// BOS & MS OS 2.0 (Verified Byte-by-Byte)
+// MS OS 2.0 Descriptor Set (The Most Robust Version)
 //--------------------------------------------------------------------+
 #define VENDOR_REQUEST_MS 0x01
 
 uint8_t const desc_ms_os_20[] = {
-  // Set header: size=10, type=MS_OS_20_SET_HEADER_DESCRIPTOR, windows_version=0x06030000, total_len=162
+  // Set header: size=10, type=0, windows_version=0x06030000, total_len=162
   0x0A, 0x00, 0x00, 0x00, 0x00, 0x00, 0x03, 0x06, 0xA2, 0x00,
-  // Configuration subset header: size=8, type=MS_OS_20_SUBSET_HEADER_CONFIGURATION, length=152
+  // Configuration subset header: size=8, type=1, config_index=0, length=152
   0x08, 0x00, 0x01, 0x00, 0x00, 0x00, 0x98, 0x00,
-  // Function subset header: size=8, type=MS_OS_20_SUBSET_HEADER_FUNCTION, interface=0, length=144
+  // Function subset header: size=8, type=2, first_interface=0, length=144
   0x08, 0x00, 0x02, 0x00, 0x00, 0x00, 0x90, 0x00,
-  // Compatible ID: size=20, type=MS_OS_20_FEATURE_COMPATBLE_ID, ID=WINUSB
+  // Compatible ID: size=20, type=3, ID=WINUSB
   0x14, 0x00, 0x03, 0x00, 'W', 'I', 'N', 'U', 'S', 'B', 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-  // Registry property: size=124, type=MS_OS_20_FEATURE_REG_PROPERTY, data_type=REG_SZ, name_len=42, name="DeviceInterfaceGUID"
+  // Registry property: size=124, type=4, data_type=1 (REG_SZ), name_len=42, name="DeviceInterfaceGUID"
   0x7C, 0x00, 0x04, 0x00, 0x01, 0x00, 0x2A, 0x00,
   'D', 0, 'e', 0, 'v', 0, 'i', 0, 'c', 0, 'e', 0, 'I', 0, 'n', 0, 't', 0, 'e', 0, 'r', 0, 'f', 0, 'a', 0, 'c', 0, 'e', 0, 'G', 0, 'U', 0, 'I', 0, 'D', 0, 0, 0,
   0x50, 0x00,
@@ -94,13 +92,20 @@ uint8_t const desc_ms_os_20[] = {
 };
 
 uint8_t const desc_bos[] = {
+    // BOS Header
     5, TUSB_DESC_BOS, 33, 0x00, 1, 
+    // Platform Capability Descriptor (28 bytes)
     28, TUSB_DESC_DEVICE_CAPABILITY, 0x05, 0x00,
-    0xDF, 0x60, 0xDD, 0xD8, 0x89, 0x45, 0xC7, 0x4C, 0x9C, 0xD2, 0x65, 0x9D, 0x9E, 0x64, 0x8A, 0x9F,
-    0x00, 0x00, 0x03, 0x06, sizeof(desc_ms_os_20), 0x00, VENDOR_REQUEST_MS, 0x00
+    // MS OS 2.0 UUID: D8DD3967-A310-42FA-9562-373D7E7A5D7C (Microsoft Standard)
+    0x67, 0x39, 0xDD, 0xD8, 0x10, 0xA3, 0xFA, 0x42, 0x95, 0x62, 0x37, 0x3D, 0x7E, 0x7A, 0x5D, 0x7C,
+    0x00, 0x00, 0x03, 0x06, // dwWindowsVersion
+    0xA2, 0x00,             // wMSOSDescSetTotalLength (162 bytes = 0xA2)
+    VENDOR_REQUEST_MS, 0x00 // bMS_VendorCode, bAltEnumCode
 };
 
-uint8_t const * tud_descriptor_bos_cb(void) { return desc_bos; }
+uint8_t const * tud_descriptor_bos_cb(void) {
+    return desc_bos;
+}
 
 bool tud_vendor_control_xfer_cb(uint8_t rhport, uint8_t stage, tusb_control_request_t const * request) {
     if (stage != CONTROL_STAGE_SETUP) return true;
